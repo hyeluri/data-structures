2a2fbece4527dd58369c0c8bcb2878e399840f3f
Hack Reactor Students <students@hackreactor.com>
2014-02-26 16:19:53 -0800

    finished up to functional-shared stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,33 @@
 var makeStack = function() {
   // Hey! Copy your code from src/functional/stack.js and paste it here
-};
+  var instance = {};
+  instance.sizeObj = 0;
+  instance.storage = {};
+
+  return _.extend(instance, stackMethods);
   
-var stackMethods = {};
+};
+
+var stackMethods = {
+
+  push:function(value){
+    this.storage[this.sizeObj++] = value;
+  },
+
+  pop:function(){
+    var item;
+    item = this.storage[this.sizeObj-1]
+    
+    if(item != undefined){
+      delete this.storage[this.sizeObj-1];
+      this.sizeObj--;
+    }
+
+    return item;
+  },
+
+  size:function(){
+    return this.sizeObj;
+  }
+
+};
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var newest = 0;
+  var oldest = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[newest++] = value;
   };
 
   instance.dequeue = function(){
+    var item = storage[oldest];
+    if(item != undefined){
+      delete storage[oldest];
+      oldest++;
+      return item;
+    }
   };
 
   instance.size = function(){
+    return newest - oldest;
+
   };
 
   return instance;
