bb73a6261d96e2ec080ea73129188b423a76c9b8
Hack Reactor Students <students@hackreactor.com>
2014-02-28 16:05:12 -0800

    completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,16 +1,24 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item){
+  if(this._storage.indexOf(item) !== -1){
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  if(this.contains(item)){
+    this._storage.splice(this._storage.indexOf(item),1);
+  }
 };
